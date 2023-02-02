9e2e34c509fae637d51d5d7dbe42860ef35c0e31
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-02 22:53:00 +0000

    Ref(app.*): Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainsComponent } from './trains/trains.component';
+import { MaterialModule } from 'src/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
